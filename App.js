f3e2ec2674550966ffe3615b970e95bf06454a32
cfrog1 <conorrogers65@gmail.com>
2021-09-27 15:52:13 +1000

    Changed navigation to tabs
    changed navigation to tabs, with tabs for map, notificaiton and settings

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MapScreen from './screens/MapScreen';
 
-import PlaceNavigator from './navigator/PlaceNavigator';
+function NotificationScreen() {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Notifications</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return(
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 
 export default function App() {
-  return <PlaceNavigator />;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Map" component={MapScreen} 
+        options={{tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="map-marker" color={color} size={size} />
+          ),}} />
+        <Tab.Screen name="Notifications" component={NotificationScreen}
+        options={{tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="bell" color={color} size={size} />
+        ),}} />
+        <Tab.Screen name="Settings" component={SettingsScreen}
+        options={{tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="cog" color={color} size={size} />
+        ),}} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 
